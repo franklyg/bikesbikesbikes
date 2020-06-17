8c198b8c1e6af4deb8fec9adf53b1eb46231b647
fgisonni <francesco@mindfulhealth.com>
2020-06-17 18:58:28 -0400

    updated css

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "87d9b9a20373aaf341cb";
+/******/ 	var hotCurrentHash = "261cdaf5716bf6b1730b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
