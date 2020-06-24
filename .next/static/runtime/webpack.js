3ef4c227fe33ab7090cd53ffedf568aad83c60eb
franklyg <f.gisonni@gmail.com>
2020-06-24 11:45:24 -0400

    put transition classes before purge

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0760b1d4cd9c90f4e5e8";
+/******/ 	var hotCurrentHash = "5d53a101caceb53d1343";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
