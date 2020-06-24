78d16fb9f68a2cc3d7c0a2925f647c33680863f9
franklyg <f.gisonni@gmail.com>
2020-06-24 11:24:33 -0400

    updated styles and added lazy load to all pafge compnents

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1b6de6c8dfc69dc8f4a8";
+/******/ 	var hotCurrentHash = "13ea37f2665d2321dea4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/product-page.js
+++ components/product-page.js
@@ -29,13 +29,13 @@ class ProductCard extends React.Component{
                     return(
                         <a href={card.product_card_link.url} className="shadow-small product-card hover:shadow-medium transition-shadow duration-200 border-solid border-2 border-gray-100 " key={cardIndex} target="_blank">
                         <LazyLoad key={cardIndex} height={200} offset={[-100, 0]}>
-                        <CSSTransition
-                          key={cardIndex}
-                          in={true}
-                          appear={true}
-                          timeout={300}
-                          classNames="transition"
-                        >
+                          <CSSTransition
+                            key={cardIndex}
+                            in={true}
+                            appear={true}
+                            timeout={1000}
+                            classNames="transition"
+                            >
                             <div>
                               <div className="product-card-image h-64 md:h-56 lg:h-40 xl:h-64 pb-6" style={{background: 'url('+card.product_card_image.url+')', backgroundSize: 'cover', backgroundPosition: 'center' }}>
                               </div>
@@ -48,13 +48,12 @@ class ProductCard extends React.Component{
                                 </div>
                               </div>
                             </div>
-                        </CSSTransition>
-                        </LazyLoad>
-                            <div className="buy-tag font-bold py-2 px-4 font-abel rounded-bl-lg grid grid-cols-2 items-center justify-end  hidden md:flex">
-                              <span className="col-span-1">View Now</span>
-                              <svg xmlns="http://www.w3.org/2000/svg" className="arrow w-4 h-4 col-span-1" viewBox="0 0 24 24"><path d="M17.428 5h-17.428l6.616 7-6.616 7h17.428l6.572-7z"/></svg>
-                            </div>
-
+                            </CSSTransition>
+                          </LazyLoad>
+                          <div className="buy-tag font-bold py-2 px-4 font-abel rounded-bl-lg grid grid-cols-2 items-center justify-end  hidden md:flex">
+                            <span className="col-span-1">View Now</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="arrow w-4 h-4 col-span-1" viewBox="0 0 24 24"><path d="M17.428 5h-17.428l6.616 7-6.616 7h17.428l6.572-7z"/></svg>
+                          </div>
                         </a>
                       )
                     })}
@@ -68,16 +67,26 @@ class ProductCard extends React.Component{
 
               const productSpecificItem = slice.fields.map(function(pageSpecific, pageSpecificIndex){
                   return (
-                    <a href={`${RichText.asText(pageSpecific.product_page_link)}`} className="specific-product-item grid md:grid-cols-2 grid-cols-1 flex items-center shadow-small hover:shadow-medium transition-shadow duration-200 xl:mb-8 lg:mb-4 md:mb-4 mb-4" key={pageSpecificIndex}>
-                      <div className="product-card-image lg:h-110 sm:h-64 h-64" style={{background: 'url('+pageSpecific.product_image.url +')'}}>
-                      </div>
-                      <div className="specific-item-content text-6xl leading-snug p-4 text-center flex grid grid-cols-8 items-center justify-end lg:h-110 sm:h-64 h-64">
-                        <div className="md:col-span-6 col-span-8 font-poppins font-bold">
-                          <RichText render={pageSpecific.product_page_title}/>
+                    <LazyLoad key={pageSpecificIndex} height={200} offset={[-100, 0]}>
+                      <CSSTransition
+                        key={pageSpecificIndex}
+                        in={true}
+                        appear={true}
+                        timeout={1000}
+                        classNames="transition"
+                      >
+                      <a href={`${RichText.asText(pageSpecific.product_page_link)}`} className="specific-product-item grid md:grid-cols-2 grid-cols-1 flex items-center shadow-small hover:shadow-medium transition-shadow duration-200 xl:mb-8 lg:mb-4 md:mb-4 mb-4" key={pageSpecificIndex}>
+                        <div className="product-card-image lg:h-110 sm:h-64 h-64" style={{background: 'url('+pageSpecific.product_image.url +')'}}>
                         </div>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="arrow w-34 h-16 md:col-span-1 col-span-8 md:block hidden" viewBox="0 0 24 24"><path d="M17.428 5h-17.428l6.616 7-6.616 7h17.428l6.572-7z"/></svg>
-                      </div>
-                    </a>
+                        <div className="specific-item-content text-6xl leading-snug p-4 text-center flex grid grid-cols-8 items-center justify-end lg:h-110 sm:h-64 h-64">
+                          <div className="md:col-span-6 col-span-8 font-poppins font-bold">
+                            <RichText render={pageSpecific.product_page_title}/>
+                          </div>
+                          <svg xmlns="http://www.w3.org/2000/svg" className="arrow w-34 h-16 md:col-span-1 col-span-8 md:block hidden" viewBox="0 0 24 24"><path d="M17.428 5h-17.428l6.616 7-6.616 7h17.428l6.572-7z"/></svg>
+                        </div>
+                      </a>
+                    </CSSTransition>
+                  </LazyLoad>
                     );
 
                   });
@@ -87,13 +96,23 @@ class ProductCard extends React.Component{
                 } else if(slice.__typename === 'Product_pageBodyPage_online_advert') {
                   const onlineAdvert = slice.fields.map(function(advertElem, advertElemIndex){
                     return (
-                      <div className="flex justify-center xl:mb-8 lg:mb-4 md:mb-4 mb-4" key={advertElemIndex}>
-                        <a href={advertElem.page_advert_link.url} className="online-advert shadow-small" key={advertElemIndex} target="_blank">
-                          <CoverImage
-                            url={advertElem.page_advert_image.url}
-                          />
-                        </a>
-                      </div>
+                      <LazyLoad key={advertElemIndex} height={200} offset={[-100, 0]}>
+                        <CSSTransition
+                        key={advertElemIndex}
+                        in={true}
+                        appear={true}
+                        timeout={1000}
+                        classNames="transition"
+                        >
+                        <div className="flex justify-center xl:mb-8 lg:mb-4 md:mb-4 mb-4" key={advertElemIndex}>
+                          <a href={advertElem.page_advert_link.url} className="online-advert shadow-small" key={advertElemIndex} target="_blank">
+                            <CoverImage
+                              url={advertElem.page_advert_image.url}
+                            />
+                          </a>
+                        </div>
+                        </CSSTransition>
+                      </LazyLoad>
                     );
                   });
                   return onlineAdvert;
