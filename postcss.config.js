6223ada1c7ce190e2ac65d89b22c4f793143625a
franklyg <f.gisonni@gmail.com>
2020-06-24 11:59:15 -0400

    added purge

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,7 @@ module.exports = {
     'tailwindcss',
     process.env.NODE_ENV === 'production'
       ? [
-          // '@fullhuman/postcss-purgecss',
+          '@fullhuman/postcss-purgecss',
           {
             content: [
               './pages/**/*.{js,jsx,ts,tsx}',
