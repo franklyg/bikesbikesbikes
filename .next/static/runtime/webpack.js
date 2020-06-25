2f1c2a567a639e0cdb960b5d1f55f7d000eb28e0
franklyg <f.gisonni@gmail.com>
2020-06-25 11:35:34 -0400

    updated posts

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "14c6dd3f216cbfe6d9d9";
+/******/ 	var hotCurrentHash = "14dac787978b86bd5a74";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
